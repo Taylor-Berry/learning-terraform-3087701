1839b4560ceceecfe3511a63f27764d022443b22
Taylor Berry <39883174+Taylor-Berry@users.noreply.github.com>
2025-02-03 09:24:54 -0500

    typo fix

--- modules/blog/main.tf
+++ modules/blog/main.tf
@@ -86,9 +86,8 @@ module "blog_alb" {
 
 resource "aws_autoscaling_attachment" "blog_asg_attachment" {
   autoscaling_group_name = module.autoscaling.autoscaling_group_name
-  lb_target_group_arn    = module.blog_alb.lb_target_group_arn[0]  # ✅ Correct
+  lb_target_group_arn    = module.blog_alb.target_group_arns[0]
 }
-
 
 module "blog_sg" {
   source = "terraform-aws-modules/security-group/aws"
