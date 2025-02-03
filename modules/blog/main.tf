145305c9da08763439b8f79d49575da55b4fb8d9
Taylor Berry <39883174+Taylor-Berry@users.noreply.github.com>
2025-02-03 10:19:10 -0500

    last fix?

--- modules/blog/main.tf
+++ modules/blog/main.tf
@@ -75,6 +75,7 @@ module "blog_alb" {
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
+      target_id        = module.autoscaling.autoscaling_group_arn
     }
   }
 
