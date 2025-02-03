85739aeed6e9d5d5a9e92ecb64989ad012c4c2b0
Taylor Berry <39883174+Taylor-Berry@users.noreply.github.com>
2025-02-03 10:30:01 -0500

    try try try again

--- modules/blog/outputs.tf
+++ modules/blog/outputs.tf
@@ -1,3 +1,8 @@
 output "environment_url" {
     value = module.blog_alb.dns_name
 }
+
+output "target_group_arns" {
+  description = "List of ALB Target Group ARNs"
+  value       = aws_lb_target_group.this[*].arn
+}
